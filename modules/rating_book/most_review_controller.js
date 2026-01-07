a13feccf5c18a7fbae1ff54941ea3a07fc7ca971
phuowqky <phuongtruongky14012004@gmail.com>
2026-01-07 21:09:23 +0700

    commit fix ranking

--- modules/rating_book/most_review_controller.js
+++ modules/rating_book/most_review_controller.js
@@ -1,3 +1,54 @@
+// import Review from "../../models/review_book_model.js";
+// import Book from "../../models/book.js";
+
+// export const getMostReviewedBooks = async (req, res) => {
+//   try {
+//     const mostReviewed = await Review.aggregate([
+//       {
+//         $group: {
+//           _id: "$bookId",
+//           reviewCount: { $sum: 1 },
+//           avgRating: { $avg: "$rating" },
+//         },
+//       },
+//       { $sort: { reviewCount: -1 } },
+//       { $limit: 5 }, // 👉 chỉ lấy 5 sách nhiều review nhất
+//       {
+//         $lookup: {
+//           from: "books",
+//           localField: "_id",
+//           foreignField: "_id",
+//           as: "bookInfo",
+//         },
+//       },
+//       { $unwind: "$bookInfo" },
+//       {
+//         $project: {
+//           _id: 0,
+//           bookId: "$bookInfo._id",
+//           title: "$bookInfo.title",
+//           author: "$bookInfo.author",
+//           category: "$bookInfo.category",
+
+//           reviewCount: 1,
+//           avgRating: { $round: ["$avgRating", 1] }, // làm tròn 1 chữ số
+//         },
+//       },
+//     ]);
+
+//     res.status(200).json({
+//       success: true,
+//       message: "Top 5 sách có nhiều đánh giá nhất",
+//       data: mostReviewed,
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
+
 import Review from "../../models/review_book_model.js";
 import Book from "../../models/book.js";
 
@@ -12,10 +63,10 @@ export const getMostReviewedBooks = async (req, res) => {
         },
       },
       { $sort: { reviewCount: -1 } },
-      { $limit: 5 }, // 👉 chỉ lấy 5 sách nhiều review nhất
+      { $limit: 5 },
       {
         $lookup: {
-          from: "books",
+          from: "books", // ⚠️ đúng tên collection
           localField: "_id",
           foreignField: "_id",
           as: "bookInfo",
@@ -25,12 +76,21 @@ export const getMostReviewedBooks = async (req, res) => {
       {
         $project: {
           _id: 0,
+
+          // id sách
           bookId: "$bookInfo._id",
+
+          // thông tin sách
           title: "$bookInfo.title",
           author: "$bookInfo.author",
           category: "$bookInfo.category",
+
+          // ✅ ảnh bìa sách
+          coverImage: "$bookInfo.coverImage",
+
+          // thống kê
           reviewCount: 1,
-          avgRating: { $round: ["$avgRating", 1] }, // làm tròn 1 chữ số
+          avgRating: { $round: ["$avgRating", 1] },
         },
       },
     ]);
